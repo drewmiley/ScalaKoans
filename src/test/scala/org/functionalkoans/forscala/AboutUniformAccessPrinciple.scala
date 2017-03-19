cfeed2956974ceae75682d8051df13ff5b050018
Drew Miley <dmiley@scottlogic.co.uk>
2017-03-19 17:40:16 +0000

    AboutUniformAccessPrinciple Complete

--- src/test/scala/org/functionalkoans/forscala/AboutUniformAccessPrinciple.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutUniformAccessPrinciple.scala
@@ -19,12 +19,12 @@ class AboutUniformAccessPrinciple extends KoanSuite {
 
     koan("Can access age as parameterless method") {
         val me = new CalculatesAgeUsingMethod(2010, 2003)
-        me.age should be(__)
+        me.age should be(7)
     }
 
     koan("Can access age as property") {
         val me = new CalculatesAgeUsingProperty(2010, 2003)
-        me.age should be(__)
+        me.age should be(7)
     }
 
     koan("Cannot add parameter to Method invocation") {
@@ -32,21 +32,19 @@ class AboutUniformAccessPrinciple extends KoanSuite {
         // uncomment following line to see what happens if you try to access parameterless method with parens
         //me.age() should be (7)
     }
+
     koan("What happens when I update current year using property") {
         val me = new CalculatesAgeUsingProperty(2010, 2003)
 
-
         me.currentYear = 2011
-        me.age should be(__)
+        me.age should be(7)
     }
 
     koan("What happens when I update current year using method") {
         val me = new CalculatesAgeUsingMethod(2010, 2003)
 
-
         me.currentYear = 2011
-        me.age should be(__)
+        me.age should be(8)
     }
-
 
 }
